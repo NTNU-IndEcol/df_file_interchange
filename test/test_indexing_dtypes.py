d1fbc3c92a25f47541e1f8c76cd11fad7668e201
EsmeMaxwell <esme@allicient.co.uk>
2024-03-04 04:21:22 +0100

    initial test setup

--- test/test_indexing_dtypes.py
+++ test/test_indexing_dtypes.py
@@ -11,11 +11,11 @@ import pytest
 import pandas as pd
 import numpy as np
 
-import df_file_interchange as fi
 
 TESTPATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(TESTPATH, ".."))
 
+import df_file_interchange as fi
 
 
 @pytest.fixture()
@@ -23,6 +23,19 @@ def std_indices():
     return fi.fi_generic._generate_example_indices()
 
 
-def test_save_load_indices(std_indices):
+def test_save_load_indices(tmp_path: Path, std_indices):
     dfs = fi.fi_generic._generate_dfs_from_indices(std_indices)
-    pass
+    
+    for idx, df in dfs.items():
+        print(f"Testing {idx}")
+
+        # Generate and save
+        target_datafile_csv = tmp_path / f"test_df_{idx}__csv.csv"
+        target_metafile_csv = tmp_path / f"test_df_{idx}__csv.yaml"
+        metafile_csv = fi.write_df_to_fi_generic(df, target_datafile_csv, target_metafile_csv, fi.FIFileFormatEnum.csv)
+        (df_reload_csv, metainfo_reload_csv) = fi.read_fi_to_df_generic(metafile_csv)
+
+        # Compare
+        pd._testing.assert_frame_equal(df, df_reload_csv)
+
+
