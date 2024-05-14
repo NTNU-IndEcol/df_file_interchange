05f90b9d1fb1600361b9d050c91630c0a4e2a111
EsmeMaxwell <esme@allicient.co.uk>
2024-05-14 12:36:47 +0200

    Ruff check and format

--- test/test_custom_info_extend_ci.py
+++ test/test_custom_info_extend_ci.py
@@ -13,7 +13,6 @@ import numpy as np
 import df_file_interchange as fi
 from df_file_interchange.file.rw import chk_strict_frames_eq_ignore_nan
 from df_file_interchange.ci.unit.base import FIBaseUnit
-from df_file_interchange.ci.unit.currency import FICurrencyUnit
 from df_file_interchange.ci.extra.std_extra import FIStdExtraInfo
 from df_file_interchange.ci.structured import FIStructuredCustomInfo
 
@@ -26,21 +25,25 @@ class FITestUnit(FIBaseUnit):
 class FITestExtraInfo(FIStdExtraInfo):
     hamster_collective: str | None = None
 
+
 class FITestStructuredCustomInfo(FIStructuredCustomInfo):
     hamster_motto: str | None = None
 
 
-
 def test_save_load_with_extend_extra_info(tmp_path: Path):
-
     # Create dataframe with extended custom info classes
-    df = pd.DataFrame(np.random.randn(4, 3), columns=["pet hamsters", "wild hamsters", "space hamsters"])
+    df = pd.DataFrame(
+        np.random.randn(4, 3),
+        columns=["pet hamsters", "wild hamsters", "space hamsters"],
+    )
 
     unit_cur_pet = FITestUnit(unit_desc="hamster", hamster_type="syrian")
     unit_cur_wild = FITestUnit(unit_desc="hamster", hamster_type="syrian")
     unit_cur_space = FITestUnit(unit_desc="hamster", hamster_type="space-hamster")
 
-    extra_info = FITestExtraInfo(author="Spud", source="Potato", hamster_collective="going nuts")
+    extra_info = FITestExtraInfo(
+        author="Spud", source="Potato", hamster_collective="going nuts"
+    )
 
     custom_info = FITestStructuredCustomInfo(
         extra_info=extra_info,
@@ -49,7 +52,7 @@ def test_save_load_with_extend_extra_info(tmp_path: Path):
             "unit_cur_wild": unit_cur_wild,
             "unit_cur_space": unit_cur_space,
         },
-        hamster_motto="jump, baby"
+        hamster_motto="jump, baby",
     )
 
     # Save CSV
@@ -86,5 +89,9 @@ def test_save_load_with_extend_extra_info(tmp_path: Path):
     assert isinstance(metainfo_reload.custom_info, FITestStructuredCustomInfo)
     assert isinstance(metainfo_reload.custom_info.extra_info, FITestExtraInfo)
     assert isinstance(metainfo_reload.custom_info.col_units["unit_cur_pet"], FITestUnit)
-    assert isinstance(metainfo_reload.custom_info.col_units["unit_cur_wild"], FITestUnit)
-    assert isinstance(metainfo_reload.custom_info.col_units["unit_cur_space"], FITestUnit)
+    assert isinstance(
+        metainfo_reload.custom_info.col_units["unit_cur_wild"], FITestUnit
+    )
+    assert isinstance(
+        metainfo_reload.custom_info.col_units["unit_cur_space"], FITestUnit
+    )
